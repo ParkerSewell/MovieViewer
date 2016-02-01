bad4faab3fbd47b6a046c3736d249fd99a53456a
ParkerSewell <parkersewell@yahoo.com>
2016-01-31 20:09:55 -0600

    Added cool features

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -8,22 +8,48 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var networkError: UIView!
+    @IBOutlet weak var searchBar: UISearchBar!
 
+    
     var movies: [NSDictionary]?
+    var filterMovies: [NSDictionary]?
+    
+    //Check this last bracket... May be wrong
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
-        tableView.delegate = self
+        searchBar.delegate = self
         
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        searchBar.endEditing(true)
+    }
+    
+    
+    
+    //Check this func... May be wrong down the road
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+
+
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(
+ 
             URL: url!,
             cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
             timeoutInterval: 10)
@@ -34,34 +60,73 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue: NSOperationQueue.mainQueue()
         )
         
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
         let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             print("response: \(responseDictionary)")
+                             MBProgressHUD.hideHUDForView(self.view, animated: true)
+                            
                         
                             self.movies = responseDictionary["results"]as? [NSDictionary]
+                            self.filterMovies = self.movies!
                             self.tableView.reloadData()
+                            refreshControl.endRefreshing()
                             
                     }
+                }
+                else {
+                    print("There is an error")
+                    self.networkError.hidden = false
                 }
         })
         task.resume()
         
-        // Do any additional setup after loading the view.
     }
+    
+        
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String){
+        if searchText.isEmpty {
+            filterMovies = movies
+        } else {
+            filterMovies = movies!.filter({ (movie: NSDictionary) -> Bool in
+                if let title = movie["title"] as? String {
+                    if title.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                        return  true
+                    } else {
+                        return false
+                    }
+                }
+                return false
+            })
+        }
+        tableView.reloadData()
+        
+
+    
+    // Do any additional setup after loading the view.
+
+            
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
   
         // Dispose of any resources that can be recreated.
         
+        
+        
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let movies = movies {
-            return movies.count
+        if let filterMovies = filterMovies{
+            return filterMovies.count
             
         }else{
             return 0
@@ -71,7 +136,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as! MovieTableViewCell
-        let movie = movies![indexPath.row]
+        let movie = filterMovies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let posterPath = movie["poster_path"] as! String
@@ -89,6 +154,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
             
     }
+
+
 
 
     /*
